fa0390b59ddda8eba2bec58bd525738f9c279c1c
Helderk1 <helderk2011@hotmail.com>
2021-12-01 20:28:32 -0300

    first commit

---
+++ 01_Aula_Construtores/Produto.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Globalization;
+
+namespace _01_Aula_Construtores
+{
+    class Produto
+    {
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+
+        public Produto (string nome, double preco)
+        {
+            _nome = nome;
+            _preco = preco;
+        }
+        public Produto(string nome, double preco, int quantidade) : this ( nome, preco)
+        {
+            _quantidade = quantidade;
+        }
+        public double ValorTotalEmEstoque()
+        {
+            return _preco * _quantidade;
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            _quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            _quantidade -= quantidade;
+        }
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+        public double Preco
+        {
+            get
+            {
+                return _preco;
+            }
+        }
+        public override string ToString()
+        {
+            return _nome
+            + ", $ "
+            + _preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + _quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ 01_Aula_Construtores/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+
+namespace _01_Aula_Construtores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+            Produto p = new Produto(nome, preco, quantidade);
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+            Console.ReadKey();
+        }
+    }
+}
+/*
+ * Formas para instansiar o Produto
+ *             Produto p = new Produto(nome, preco, quantidade);
+ *             
+ *             Produto p = new Produto();
+ *             
+ *             Produto p = new Produto
+ *             {
+ *             Nome = "TV", 
+ *             Preco = 900.00,
+ *             Quantidade = 10
+ *             };
+ */
